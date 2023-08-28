71a59290d7bee424bdb5bda62c483894a4bb1e05
Kevin Allioli <kevin@linit.io>
2023-08-28 15:50:11 +0200

    Update for win10 workflow

--- build_windows_image.ps1
+++ build_windows_image.ps1
@@ -4,9 +4,9 @@ Import-Module .\windows-imaging-tools\UnattendResources\ini.psm1
 
 $type=$args[0]
 $release=$args[1]
-$edition=$args[2]
 
-$ConfigFilePath = ".\config\$type\config-$release-$edition.ini"
+$ConfigFilePath = ".\config\$type\config-$release.ini"
+Get-WimFileImagesInfo
 Set-IniFileValue -Path (Resolve-Path $ConfigFilePath) -Section "DEFAULT" `
                                       -Key "wim_file_path" `
                                       -Value "D:\windows-sources\$release\sources\install.wim"
