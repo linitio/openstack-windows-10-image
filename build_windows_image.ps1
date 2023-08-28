f166b90b3ed9b07786b694a9293238bc2d80bf3d
Kevin Allioli <kevin@linit.io>
2023-08-28 15:54:35 +0200

    Update build_windows_image.ps1

--- build_windows_image.ps1
+++ build_windows_image.ps1
@@ -6,7 +6,7 @@ $type=$args[0]
 $release=$args[1]
 
 $ConfigFilePath = ".\config\$type\config-$release.ini"
-Get-WimFileImagesInfo
+Get-WimFileImagesInfo D:\windows-sources\$release\sources\install.wim
 Set-IniFileValue -Path (Resolve-Path $ConfigFilePath) -Section "DEFAULT" `
                                       -Key "wim_file_path" `
                                       -Value "D:\windows-sources\$release\sources\install.wim"
